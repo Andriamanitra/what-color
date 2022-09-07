cdacd7effcd77d77a77d955ed44a8b68d7894ae2
Andriamanitra <mikko77792@gmail.com>
2022-09-07 15:04:48 +0300

    annoy users who submit hex color codes

--- static/main.js
+++ static/main.js
@@ -1,3 +1,5 @@
+const HEX_COLOR_REGEX = /^#(?:[A-Fa-f0-9]{3}){1,2}$/;
+
 const cform = document.querySelector('form');
 function next() {
   let r = parseInt(256 * Math.random());
@@ -7,11 +9,17 @@ function next() {
   document.querySelector('#green').value = g;
   document.querySelector('#blue').value = b;
   document.querySelector('#name').value = '';
-  document.documentElement.style.setProperty('--color', `rgb(${r},${g},${b})`)
+  document.documentElement.style.setProperty('--color', `rgb(${r},${g},${b})`);
 }
 cform.onsubmit = function (ev) {
-  fetch('', { method: 'POST', body: new FormData(cform) });
-  next();
   ev.preventDefault();
+  const formData = new FormData(cform);
+  const name = formData.get("name");
+  if (formData.get("name").match(HEX_COLOR_REGEX)) {
+    alert("Very clever, but we are asking for color names in natural language!");
+    return;
+  }
+  fetch('', { method: 'POST', body: formData });
+  next();
 }
 next()
